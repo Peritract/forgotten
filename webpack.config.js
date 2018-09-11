11d43f5c6173e241b706cf0453bf5be3f73060a3
Peritract <peritract@hotmail.co.uk>
2018-09-11 19:44:56 +0100

    Connections

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/scripts/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.bundle.js'
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+		filename: "index.html",
+		template: './src/templates/index.html',
+		favicon: './src/assets/octo.ico'
+	})
+  ],
+  module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader'
+          ]
+        },
+       {
+         test: /\.(woff|woff2|eot|ttf|otf)$/,
+         use: [
+           'file-loader'
+         ]
+       }
+      ]
+    }
+}
